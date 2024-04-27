89f3eb8e3c511c6d599b8d3f37b80ae41de2af86
men3sgit <menesforenglish102@gmail.com>
2024-04-28 00:05:23 +0700

    [menes][new] init project

---
+++ back-end/src/test/java/vn/edu/nlu/web/Chat/ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.nlu.web.Chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

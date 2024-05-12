e25f68b53e356aa5e5cd60d7bc1f4a2a4aecf483
men3sgit <menesforenglish102@gmail.com>
2024-05-12 13:22:46 +0700

    [menes][update] push to pull

--- back-end/src/main/java/vn/edu/nlu/web/chat/model/File.java
+++ back-end/src/main/java/vn/edu/nlu/web/chat/model/File.java
@@ -4,8 +4,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.PostPersist;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 @Entity
 @Table(name = "files")
